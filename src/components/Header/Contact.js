e7b0c25d2e4d55d6bd5464d464ed56b1bf4a6ab9
Drew Bradley <williamandrewb@gmail.com>
2021-02-02 15:07:17 -0700

    Add Header

---
+++ src/components/Header/Contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Contact = () => {
+  return(
+    <section className="contact">
+      <p>email: williamandrewb@gmail.com</p>
+      <p>phone: 970-778-5061</p>
+      <p>github: github.com/DrewBradley</p>
+      <p>linkedin: linkedIn.com/DrewBradley</p>
+    </section>
+  )
+}
+
+export default Contact;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Contact from "./Contact"
+import './Header.css'
+
+const Header = () => {
+  return(
+    <header>
+      <Contact />
+    </header>
+  )
+}
+
+export default Header;
