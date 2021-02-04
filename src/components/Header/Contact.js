4e9bd78c16da9645e3414964f918c72fe8e3c443
Drew Bradley <williamandrewb@gmail.com>
2021-02-04 11:58:50 -0700

    Update CSS.

--- src/components/Header/Contact.js
+++ src/components/Header/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
       <p className="contact-deet">email: williamandrewb@gmail.com</p>
       <p className="contact-deet">phone: 970-778-5061</p>
       <p className="contact-deet">github: github.com/DrewBradley</p>
-      <p className="contact-deet">linkedin: linkedIn.com/DrewBradley</p>
+      <p className="contact-deet">linkedin: www.linkedin.com/in/drew-bradley-46a80918b/</p>
     </section>
   )
 }
